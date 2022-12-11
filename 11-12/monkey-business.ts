13425c80ba37a7aca29a3519c000ea1788020fc0
Fabrice Tsiava <fabrice.tsiava@gmail.com>
2022-12-11 18:53:32 +0100

    11/12 part 2 big int exceeds size

--- 11-12/monkey-business.ts
+++ 11-12/monkey-business.ts
@@ -1,10 +1,11 @@
-import { group } from "console";
 import { readFileSync } from "fs";
 import { resolve } from "path";
 
 type Worry = number;
+type BigWorry = bigint;
 type InspectOperation = (val: Worry) => Worry;
 
+type InspectOperationBig = (val: BigWorry) => BigWorry;
 type ReliefFn = (val: Worry) => Worry;
 
 function relief(val: Worry): Worry {
@@ -13,10 +14,17 @@ function relief(val: Worry): Worry {
 
 type TargetDecision = (val: Worry) => number;
 
+type TargetDecision2 = (val: BigWorry) => {
+  newVal: BigWorry;
+  dest: number;
+};
+
 type MonkeyConfig = {
   inspect: InspectOperation;
+  inspectBig: InspectOperationBig;
   relief: ReliefFn;
   decide: TargetDecision;
+  decide2: TargetDecision2;
 };
 
 class Monkey {
@@ -50,6 +58,10 @@ class Monkey {
     return this.items.map((i) => i.worry);
   }
 
+  get bigWorries() {
+    return this.items.map((i) => i.bigWorry);
+  }
+
   receive(item: Item) {
     this.items.push(item);
   }
@@ -78,6 +90,28 @@ class Monkey {
     }
     this._log && console.log(`monkey ${this._id} ends`);
   }
+
+  turnV2() {
+    let item: Item | undefined;
+    this._log && console.log(`monkey ${this._id} plays`);
+    while ((item = this.items.shift())) {
+      this._log && console.log("begin proceed item", item.bigWorry);
+      this._inspected++;
+      item.bigWorry = this.config.inspectBig(item.bigWorry);
+      this._log && console.log("after inspect worry =", item.bigWorry);
+      const { dest: newMonkey, newVal } = this.config.decide2(item.bigWorry);
+      item.bigWorry = newVal;
+      this._log && console.log("after decide worry =", item.bigWorry);
+      this._passTo?.(item, newMonkey);
+      this._log &&
+        console.log(
+          "end proceed item",
+          item.worry,
+          `passed to monkey ${newMonkey}`
+        );
+    }
+    this._log && console.log(`monkey ${this._id} ends`);
+  }
 }
 
 class Item {
@@ -90,11 +124,22 @@ class Item {
     this._worry = w;
   }
 
+  get bigWorry(): bigint {
+    return this._bigWorry;
+  }
+
+  set bigWorry(w: bigint) {
+    this._bigWorry = w;
+  }
+
+  private _bigWorry: bigint;
+
   passTo(m: Monkey) {
     this.hist.push(m);
   }
 
   constructor(private original: Monkey, private _worry: Worry) {
+    this._bigWorry = BigInt(_worry);
     this.passTo(original);
   }
 }
@@ -110,9 +155,48 @@ function createInspect(operator: string, op2: string): InspectOperation {
   const op2Value = Number.parseInt(op2);
 
   if (Number.isInteger(op2Value)) {
-    return (val: Worry) => fn(val, op2Value);
+    return (val: Worry) => {
+      const newVal = fn(val, op2Value);
+
+      if (!Number.isSafeInteger(newVal)) {
+        throw new Error("not a safe integer " + newVal);
+      }
+      return newVal;
+    };
   } else {
-    return (val: Worry) => fn(val, val);
+    return (val: Worry) => {
+      const newVal = fn(val, val);
+
+      if (!Number.isSafeInteger(newVal)) {
+        throw new Error("not a safe integer " + newVal);
+      }
+      return newVal;
+    };
+  }
+}
+
+const BinaryOpBigFn: Record<string, Function> = {
+  "+": (op1: bigint, op2: bigint) => op1 + op2,
+  "*": (op1: bigint, op2: bigint) => op1 * op2,
+};
+
+function createInspectBig(operator: string, op2: string): InspectOperationBig {
+  const fn = BinaryOpBigFn[operator];
+
+  const op2Value = Number.parseInt(op2);
+
+  if (Number.isInteger(op2Value)) {
+    return (val: BigWorry) => {
+      const bigOP2 = BigInt(op2Value);
+      const newVal = fn(val, bigOP2);
+
+      return newVal;
+    };
+  } else {
+    return (val: BigWorry) => {
+      const newVal = fn(val, val);
+      return newVal;
+    };
   }
 }
 
@@ -124,6 +208,28 @@ function createDecider(
   return (val: Worry) => (val % divider === 0 ? truthyDest : falsyDest);
 }
 
+function createDeciderBig(
+  divider: number,
+  truthyDest: number,
+  falsyDest: number
+): TargetDecision2 {
+  const dividerBig = BigInt(divider);
+  return (val: BigWorry) => {
+    const remaining = val % dividerBig;
+    if (remaining === BigInt(0)) {
+      return {
+        newVal: val,
+        dest: truthyDest,
+      };
+    } else {
+      return {
+        newVal: val,
+        dest: falsyDest,
+      };
+    }
+  };
+}
+
 const monkeyIdRE = /(?<id>\d+):$/;
 const itemsRE = /\d+/g;
 const divisorRE = /\d+/;
@@ -131,7 +237,7 @@ const operationRE = /(?<op1>\w+) (?<operator>\+|\*) (?<op2>\w+)/;
 const trueRE = /\d+/;
 const falseRE = /\d+/;
 
-function main(filename: string, answer?: number) {
+function main(filename: string, answer?: number, version = 1) {
   const lines = readFileSync(resolve(__dirname, filename), {
     encoding: "utf-8",
   }).split(/\r|\n|\r\n/);
@@ -176,7 +282,15 @@ function main(filename: string, answer?: number) {
     });
 
     const inspectOp = createInspect(op.operator || "", op.op2 || "");
+    const inspectOpBig = createInspectBig(op.operator || "", op.op2 || "");
+
     const decider = createDecider(
+      Number(divisor),
+      Number(trueDest),
+      Number(falseDest)
+    );
+
+    const decider2 = createDeciderBig(
       Number(divisor),
       Number(trueDest),
       Number(falseDest)
@@ -186,6 +300,8 @@ function main(filename: string, answer?: number) {
       decide: decider,
       inspect: inspectOp,
       relief: relief,
+      decide2: decider2,
+      inspectBig: inspectOpBig,
     };
 
     const worries = items?.map(Number) || [];
@@ -212,40 +328,94 @@ function main(filename: string, answer?: number) {
     m.log = log;
   }
 
-  console.log("keys", [...monkeyMap.keys()]);
+  const play1 = function () {
+    const turns = 20;
+    for (let t = 0; t < turns; t++) {
+      for (let key of monkeyMap.keys()) {
+        const monkeyToPlay = monkeyMap.get(key);
+        monkeyToPlay?.turn();
+      }
 
-  const turns = 20;
-  for (let t = 0; t < turns; t++) {
-    for (let key of monkeyMap.keys()) {
-      const monkeyToPlay = monkeyMap.get(key);
-      monkeyToPlay?.turn();
+      console.log("after turn", t + 1);
+      console.group();
+      for (let key of monkeyMap.keys()) {
+        console.log(`monkey ${key}`, monkeyMap.get(key)?.worries);
+      }
+      console.groupEnd();
     }
 
-    console.log("after turn", t + 1);
-    console.group();
-    for (let key of monkeyMap.keys()) {
-      console.log(`monkey ${key}`, monkeyMap.get(key)?.worries);
+    const inspected: number[] = [];
+    for (let m of monkeyMap.values()) {
+      console.log(`monkey ${m.id} inspected ${m.inspected} items`);
+      inspected.push(m.inspected);
     }
-    console.groupEnd();
-  }
 
-  const inspected: number[] = [];
-  for (let m of monkeyMap.values()) {
-    console.log(`monkey ${m.id} inspected ${m.inspected} items`);
-    inspected.push(m.inspected);
-  }
+    inspected.sort((a, b) => b - a);
+    console.log(inspected);
+    const max2 = inspected.slice(0, 2);
+    console.log(max2);
+    const monkeyBusiness = max2.reduce((acc, val) => acc * val, 1);
+    console.log({ monkeyBusiness });
 
-  inspected.sort((a, b) => b - a);
-  console.log(inspected);
-  const max2 = inspected.slice(0, 2);
-  console.log(max2);
-  const monkeyBusiness = max2.reduce((acc, val) => acc * val, 1);
-  console.log({ monkeyBusiness });
+    if (answer && answer != monkeyBusiness) {
+      throw new Error(`expected ${answer}`);
+    }
+  };
 
-  if (answer && answer != monkeyBusiness) {
-    throw new Error(`expected ${answer}`);
+  const play2 = function () {
+    const targetTurns = [
+      1,
+      20,
+      ...[...Array(10).keys()].map((v) => (v + 1) * 1e3),
+    ];
+    const turns = 1e4;
+    for (let t = 0; t < turns; t++) {
+      for (let key of monkeyMap.keys()) {
+        const monkeyToPlay = monkeyMap.get(key);
+        monkeyToPlay?.turnV2();
+      }
+
+      console.log("after turn", t + 1);
+      if (log) {
+        console.group();
+        for (let key of monkeyMap.keys()) {
+          console.log(`monkey ${key}`, monkeyMap.get(key)?.worries);
+        }
+        console.groupEnd();
+      }
+
+      if (targetTurns.includes(t + 1)) {
+        console.log("after turn", t + 1);
+        console.group();
+
+        for (let m of monkeyMap.values()) {
+          console.log(`monkey ${m.id} inspected ${m.inspected} items`);
+        }
+        console.groupEnd();
+      }
+    }
+
+    const inspected: number[] = [];
+    for (let m of monkeyMap.values()) {
+      console.log(`monkey ${m.id} inspected ${m.inspected} items`);
+      inspected.push(m.inspected);
+    }
+
+    inspected.sort((a, b) => b - a);
+    console.log(inspected);
+    const max2 = inspected.slice(0, 2);
+    console.log(max2);
+    const monkeyBusiness = max2.reduce((acc, val) => acc * val, 1);
+    console.log({ monkeyBusiness });
+  };
+
+  if (version === 1) {
+    play1();
+  } else {
+    play2();
   }
 }
 
-main("test.txt", 10605);
-main("input.txt");
+// main("test.txt", 10605, 1);
+main("test.txt", 10605, 2);
+// main("input.txt", 119715);
