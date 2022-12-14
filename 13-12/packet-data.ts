a82adf65ec6e16ebd0cbd5ee2292c69bcc71a5ee
Fabrice Tsiava <fabrice.tsiava@gmail.com>
2022-12-14 17:16:02 +0100

    13/12 part 1

--- 13-12/packet-data.ts
+++ 13-12/packet-data.ts
@@ -1,98 +1,125 @@
-type Packet = Array<number | Packet>;
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-type List = Array<number | List>;
+type Packet = Array<number | Packet>;
 
 const LOG = true;
 
-const test1 = "[1,1,3,1,1]";
-const parse1 = parsePacket(test1, 0);
-console.log({ parse1: JSON.stringify(parse1.packet) });
+function testParse() {
+  const test1 = "[1,1,3,1,1]";
+  const parse1 = parsePacket(test1, 0);
+  console.log({ parse1: JSON.stringify(parse1.packet) });
 
-const test2 = "[[1],[2,3,4]]";
-const parse2 = parsePacket(test2, 0);
-console.log({ parse2: parse2.packet });
+  const test2 = "[[1],[2,3,4]]";
+  const parse2 = parsePacket(test2, 0);
+  console.log({ parse2: parse2.packet });
 
-const test3 = "[[1],4]";
-const parse3 = parsePacket(test3, 0);
-console.log({ parse3: parse3.packet });
+  const test3 = "[[1],4]";
+  const parse3 = parsePacket(test3, 0);
+  console.log({ parse3: parse3.packet });
 
-const test4 = "[[[]]]";
-const parse4 = parsePacket(test4, 0);
-console.log({ parse4: parse4.packet });
+  const test4 = "[[[]]]";
+  const parse4 = parsePacket(test4, 0);
+  console.log({ parse4: parse4.packet });
 
-const test5 = "[1,[2,[3,[4,[5,6,7]]]],8,9]";
-const parse5 = parsePacket(test5, 0);
-console.log({ parse5: JSON.stringify(parse5.packet) });
+  const test5 = "[1,[2,[3,[4,[5,6,7]]]],8,9]";
+  const parse5 = parsePacket(test5, 0);
+  console.log({ parse5: JSON.stringify(parse5.packet) });
+}
 
-const res1 = comparePackets(
-  parsePacket("[1,1,3,1,1]", 0).packet,
-  parsePacket("[1,1,5,1,1]", 0).packet,
-  0
-);
+function testCompare() {
+  const res1 = comparePackets(
+    parsePacket("[1,1,3,1,1]", 0).packet,
+    parsePacket("[1,1,5,1,1]", 0).packet,
+    0
+  );
 
-console.log({ res1 });
+  console.log({ res1 });
 
-const res2 = comparePackets(
-  parsePacket("[[1],[2,3,4]]", 0).packet,
-  parsePacket("[[1],4]", 0).packet,
-  0
-);
+  testPackets("[1,1,3,1,1]", "[1,1,5,1,1]", true);
 
-console.log({ res2 });
+  const res2 = comparePackets(
+    parsePacket("[[1],[2,3,4]]", 0).packet,
+    parsePacket("[[1],4]", 0).packet,
+    0
+  );
 
-const res3 = comparePackets(
-  parsePacket("[9]", 0).packet,
-  parsePacket("[[8,7,6]]", 0).packet,
-  0
-);
+  console.log({ res2 });
 
-console.log({ res3 });
+  testPackets("[[1],[2,3,4]]", "[[1],4]", true);
 
-const res4 = comparePackets(
-  parsePacket("[[4,4],4,4]", 0).packet,
-  parsePacket("[[4,4],4,4,4]", 0).packet,
-  0
-);
+  const res3 = comparePackets(
+    parsePacket("[9]", 0).packet,
+    parsePacket("[[8,7,6]]", 0).packet,
+    0
+  );
 
-console.log({ res4 });
+  console.log({ res3 });
 
-const res5 = comparePackets(
-  parsePacket("[7,7,7,7]", 0).packet,
-  parsePacket("[7,7,7]", 0).packet,
-  0
-);
+  testPackets("[9]", "[[8,7,6]]", false);
 
-console.log({ res5 });
+  const res4 = comparePackets(
+    parsePacket("[[4,4],4,4]", 0).packet,
+    parsePacket("[[4,4],4,4,4]", 0).packet,
+    0
+  );
 
-const res6 = comparePackets(
-  parsePacket("[]", 0).packet,
-  parsePacket("[3]", 0).packet,
-  0
-);
+  console.log({ res4 });
 
-console.log({ res6 });
+  testPackets("[[4,4],4,4]", "[[4,4],4,4,4]", true);
 
-const res7 = comparePackets(
-  parsePacket("[[[]]]", 0).packet,
-  parsePacket("[[]]", 0).packet,
-  0
-);
+  const res5 = comparePackets(
+    parsePacket("[7,7,7,7]", 0).packet,
+    parsePacket("[7,7,7]", 0).packet,
+    0
+  );
 
-console.log({ res7 });
+  console.log({ res5 });
 
-const res8 = comparePackets(
-  parsePacket("[1,[2,[3,[4,[5,6,7]]]],8,9]", 0).packet,
-  parsePacket("[1,[2,[3,[4,[5,6,0]]]],8,9]", 0).packet,
-  0
-);
+  testPackets("[7,7,7,7]", "[7,7,7]", false);
 
-console.log({ res8 });
+  const res6 = comparePackets(
+    parsePacket("[]", 0).packet,
+    parsePacket("[3]", 0).packet,
+    0
+  );
 
-testPackets(
-  "[1,[2,[3,[4,[5,6,7]]]],8,9]",
-  "[1,[2,[3,[4,[5,6,0]]]],8,9]",
-  false
-);
+  console.log({ res6 });
+
+  testPackets("[]", "[3]", true);
+
+  const res7 = comparePackets(
+    parsePacket("[[[]]]", 0).packet,
+    parsePacket("[[]]", 0).packet,
+    0
+  );
+
+  console.log({ res7 });
+
+  testPackets("[[[]]]", "[[]]", false);
+
+  const res8 = comparePackets(
+    parsePacket("[1,[2,[3,[4,[5,6,7]]]],8,9]", 0).packet,
+    parsePacket("[1,[2,[3,[4,[5,6,0]]]],8,9]", 0).packet,
+    0
+  );
+
+  console.log({ res8 });
+
+  testPackets(
+    "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+    "[1,[2,[3,[4,[5,6,0]]]],8,9]",
+    false
+  );
+
+  testPackets(
+    "[[5,7,[9,[],[8],10,1]]]",
+    "[[[5],[0,10,[2,10,6,0,8]]],[0,6,10,9]]",
+    false
+  );
+
+  testPackets("[[1],[2,3,4]]", "[[1],4]", true);
+}
 
 function parsePacket(
   line: string,
@@ -163,9 +190,11 @@ function testPackets(left: string, right: string, answer: boolean) {
   if (answer !== res) {
     throw new Error(`expected ${answer}`);
   }
+  console.log("\n");
 }
 
 function compareTopPackets(left: Packet, right: Packet): boolean | undefined {
+  LOG && console.log("compare top", JSON.stringify({ left, right }));
   return comparePackets(left, right, 0);
 }
 
@@ -179,12 +208,14 @@ function comparePackets(
 
   LOG &&
     console.log("compare", JSON.stringify({ left: leftVal, right: rightVal }));
-  if (!leftVal && rightVal) {
+
+  // /!\/!\ check for explicit undefined: 0 IS FALSY /!\/!\
+  if (leftVal === undefined && rightVal !== undefined) {
     return true;
-  } else if (leftVal && !rightVal) {
+  } else if (leftVal !== undefined && rightVal === undefined) {
     return false;
-  } else if (!leftVal && !rightVal) {
-    return;
+  } else if (leftVal === undefined && rightVal === undefined) {
+    return undefined;
   }
 
   if (typeof leftVal === "number" && typeof rightVal === "number") {
@@ -193,28 +224,68 @@ function comparePackets(
     } else if (leftVal > rightVal) {
       return false;
     } else {
-      // equality
+      // equality - check next values of current packets
       return comparePackets(left, right, ++index);
     }
   }
+
+  let res: boolean | undefined;
 
   if (Array.isArray(leftVal) && Array.isArray(rightVal)) {
-    const res = comparePackets(leftVal, rightVal, 0);
-    if (res !== undefined) {
-      return res;
-    } else {
-      // equality
-      return comparePackets(left, right, ++index);
+    res = comparePackets(leftVal, rightVal, 0);
+  } else if (typeof leftVal === "number" && Array.isArray(rightVal)) {
+    res = comparePackets([leftVal], rightVal, 0);
+  } else if (Array.isArray(leftVal) && typeof rightVal === "number") {
+    res = comparePackets(leftVal, [rightVal], 0);
+  }
+
+  if (res != undefined) {
+    return res;
+  } else {
+    return comparePackets(left, right, ++index);
+  }
+}
+
+function main(filename: string, answer?: number) {
+  const lines = readFileSync(resolve(__dirname, filename), {
+    encoding: "utf-8",
+  }).split(/\r|\n|\r\n/);
+
+  console.log(lines);
+  let sum = 0;
+  const validPairsIndices: number[] = [];
+  const validPairs: { left: string; right: string }[] = [];
+  let pairIndex = 0;
+  for (let i = 0; i < lines.length; i += 3) {
+    const left = lines[i];
+    const right = lines[i + 1];
+    console.log({ left, right });
+    pairIndex++;
+    if (
+      compareTopPackets(
+        parsePacket(left, 0).packet,
+        parsePacket(right, 0).packet
+      )
+    ) {
+      validPairs.push({ left, right });
+      sum += pairIndex;
+      validPairsIndices.push(pairIndex);
     }
   }
 
-  if (typeof leftVal === "number" && Array.isArray(rightVal)) {
-    return comparePackets([leftVal], rightVal, 0);
-  } else if (Array.isArray(leftVal) && typeof rightVal === "number") {
-    return comparePackets(leftVal, [rightVal], 0);
-  }
+  console.log({ sum, validPairsIndices });
 
-  throw new Error(
-    `comparison failed ${JSON.stringify({ left, right, index })}`
-  );
+  const res = validPairsIndices.reduce((acc, val) => acc + val, 0);
+  console.log(validPairs);
+  console.log(res);
+
+  if (answer && answer !== sum) {
+    throw new Error(`expected ${answer}`);
+  }
 }
+
+main("test.txt", 13);
+// 6133 is too high
+// 6117 is too high
+// 6113 is too high
+main("input.txt", 6101);
